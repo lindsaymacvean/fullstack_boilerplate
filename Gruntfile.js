5df7c2c12226c06247cea91c6fb4b19b43754be3
Lindsay <lindsaymacvean@gmail.com>
2015-02-10 14:20:03 -0800

    added live reload feature

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,60 @@
+'use strict';
+
 module.exports = function(grunt) {
 	
+	//Just in time loader for all grunt packages
+	require('jit-grunt')(grunt, {
+		express: 'grunt-express-server'
+	});
+
+	//Benchmark Grunt compile
+	require('time-grunt')(grunt);
+
+
 	//configure Grunt
 	grunt.initConfig({
 		
 		pkg: grunt.file.readJSON('package.json'),
-		//configure nodemon to start our server
-		nodemon: {
+
+		//Run the app in dev mode using Grunt
+		express: {
+			options: {
+				port: process.env.PORT || 9000
+			},
 			dev: {
-				script:'server/app.js'
+				options: {
+					script: 'server/app.js',
+					debug:true
+				}
+			}
+		},
+
+		//Open a webpage to the main view
+		open: {
+			server: {
+				url: 'http://localhost:'+(process.env.PORT || 9000)
+			}
+		},
+
+		//Watch for file changes and either reload the view or the server
+		watch: {
+			livereload: {
+				files: [
+					'client/**/**'
+				],
+				options: {
+					livereload: true
+				}
+			},
+			express: {
+				files: [
+					'server/**/**'
+				],
+				tasks: ['express:dev', 'wait'],
+				options: {
+					livereload: true,
+					nospawn: true //stops from spawning as a child process
+				}
 			}
 		},
 
@@ -30,9 +77,24 @@ module.exports = function(grunt) {
 
 	});
 
-	//load Grunt tasks
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	//Delay live reload until after server has restarted
+	grunt.registerTask('wait', function () {
+		grunt.log.ok('Waiting for the server reload...');
+		//force this task into async mode so other things can happen
+		var done = this.async();
+		setTimeout(function () {
+			grunt.log.writeln('Done waiting!');
+			done();
+		}, 1500);
+	});
 
-	//Register Task pointers for Grunt command
-	//grunt.registerTask('default', ['nodemon']);
+	grunt.registerTask('server', function () {
+		grunt.task.run([
+			'express:dev',
+			'wait',
+			'open',
+			'watch'
+			]);
+	});
+
 };
